30bfe5ce4750824e60a4fce88076223cbac7440b
Iri Bandas <ibandas@luc.edu>
2020-06-08 09:25:33 -0500

    videos with show more style

---
+++ main.js
@@ -0,0 +1,17 @@
+function showVideo1() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+function showVideo2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
