9a1914ea34f5419a787ecbce748a8a79f80d1598
Iri Bandas <ibandas@luc.edu>
2020-06-08 10:45:49 -0500

    more navbar and resume and bio

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 function showVideo1() {
     var x = document.getElementById("myDIV1");
+    var elem = document.getElementById("mybtn1");
     if (x.style.display === "block") {
       x.style.display = "none";
+      elem.innerHTML= "Show More";
     } else {
       x.style.display = "block";
+      elem.innerHTML= "Hide";
     }
 }
 
 function showVideo2() {
     var x = document.getElementById("myDIV2");
+    var elem = document.getElementById("mybtn2");
     if (x.style.display === "block") {
       x.style.display = "none";
+      elem.innerHTML= "Show More";
     } else {
       x.style.display = "block";
+      elem.innerHTML= "Hide";
     }
 }
